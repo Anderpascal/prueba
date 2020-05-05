2d707741f2280940c4bbb1e48247d12d5f890f23
Anderpascal <50362627+Anderpascal@users.noreply.github.com>
2020-05-05 16:04:17 +0200

    prueba2

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int JAJAJAJA = 2;
+        int situ = 1;
         setContentView(R.layout.activity_main);
     }
 }
