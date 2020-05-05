daf41a0451cbeb10958a138f4bcb66e21abb82bf
Anderpascal <50362627+Anderpascal@users.noreply.github.com>
2020-05-05 16:00:37 +0200

    hey

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int JAJAJAJA = 2;
         setContentView(R.layout.activity_main);
     }
 }
